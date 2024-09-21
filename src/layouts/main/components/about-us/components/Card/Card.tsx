995c4ca020a1badc8e8f7a43e28e24c7f8eca270
NeVzaimnoePadenie <118021544+AAkismetKLASNI@users.noreply.github.com>
2024-09-21 14:37:03 +0500

    change acc color and some fix

--- src/layouts/main/components/about-us/components/Card/Card.tsx
+++ src/layouts/main/components/about-us/components/Card/Card.tsx
@@ -7,7 +7,7 @@ interface CardInterface {
 
 export const Card: FC<CardInterface> = ({ text, img }) => {
   return (
-    <div className='w-[470px] relative bg-gray-light py-20 px-8 rounded-3xl'>
+    <div className='w-full lg:w-[470px] relative bg-gray-light py-20 px-8 rounded-3xl'>
       {img ? (
         <>
           <img src={img} className='absolute top-0 translate-y-[-50%]' />
